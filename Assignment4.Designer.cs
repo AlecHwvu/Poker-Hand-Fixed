42130ec555a967de294c3dfef4345dc59e03a1b1
AlecHwvu <233874714+AlecHwvu@users.noreply.github.com>
2025-11-07 19:52:56 -0500

    Add files via upload

---
+++ Assignment4.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Forms;
+
+namespace MainForm.CS
+{
+    partial class Assignment4
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Assignment4));
+
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.imgCards = new System.Windows.Forms.ImageList(this.components);
+
+            this.picCard1 = new System.Windows.Forms.PictureBox();
+            this.picCard2 = new System.Windows.Forms.PictureBox();
+            this.picCard3 = new System.Windows.Forms.PictureBox();
+            this.picCard4 = new System.Windows.Forms.PictureBox();
+            this.picCard5 = new System.Windows.Forms.PictureBox();
+
+            this.chkKeep1 = new System.Windows.Forms.CheckBox();
+            this.chkKeep2 = new System.Windows.Forms.CheckBox();
+            this.chkKeep3 = new System.Windows.Forms.CheckBox();
+            this.chkKeep4 = new System.Windows.Forms.CheckBox();
+            this.chkKeep5 = new System.Windows.Forms.CheckBox();
+
+            ((System.ComponentModel.ISupportInitialize)(this.picCard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).BeginInit();
+
+            this.SuspendLayout();
+
+            // 
+            // Buttons
+            // 
+            this.btnDeal.Location = new System.Drawing.Point(50, 20);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(75, 30);
+            this.btnDeal.Text = "&Deal";
+            this.btnDeal.UseVisualStyleBackColor = true;
+
+            this.btnSave.Location = new System.Drawing.Point(600, 20);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(80, 30);
+            this.btnSave.Text = "&Save Hand";
+            this.btnSave.UseVisualStyleBackColor = true;
+
+            this.btnLoad.Location = new System.Drawing.Point(700, 20);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(80, 30);
+            this.btnLoad.Text = "&Load Hand";
+            this.btnLoad.UseVisualStyleBackColor = true;
+
+            // 
+            // ImageList
+            // 
+            this.imgCards.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgCards.ImageStream")));
+            this.imgCards.TransparentColor = System.Drawing.Color.Transparent;
+
+            for (int i = 0; i < 52; i++)
+            {
+                this.imgCards.Images.SetKeyName(i, $"p{i}.png");
+            }
+
+            // 
+            // PictureBoxes
+            // 
+            PictureBox[] pics = { picCard1, picCard2, picCard3, picCard4, picCard5 };
+            int startX = 20, startY = 70, cardWidth = 140, cardHeight = 191, spacing = 10;
+
+            for (int i = 0; i < pics.Length; i++)
+            {
+                pics[i].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                pics[i].Location = new System.Drawing.Point(startX + i * (cardWidth + spacing), startY);
+                pics[i].Size = new System.Drawing.Size(cardWidth, cardHeight);
+                pics[i].SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                pics[i].TabStop = false;
+            }
+
+            // 
+            // CheckBoxes
+            // 
+            CheckBox[] checks = { chkKeep1, chkKeep2, chkKeep3, chkKeep4, chkKeep5 };
+            int checkY = startY - 25;
+            for (int i = 0; i < checks.Length; i++)
+            {
+                checks[i].AutoSize = true;
+                checks[i].Location = new System.Drawing.Point(startX + i * (cardWidth + spacing) + 40, checkY);
+                checks[i].Text = $"&Keep {i + 1}";
+                checks[i].UseVisualStyleBackColor = true;
+            }
+
+            // 
+            // Assignment4 Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 300);
+            this.Controls.AddRange(new System.Windows.Forms.Control[] { btnDeal, btnSave, btnLoad });
+            this.Controls.AddRange(pics);
+            this.Controls.AddRange(checks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Assignment4";
+            this.Text = "Poker Hand Simulator";
+            this.Load += new System.EventHandler(this.Assignment4_Load);
+
+            ((System.ComponentModel.ISupportInitialize)(this.picCard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCard5)).EndInit();
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ImageList imgCards;
+        private System.Windows.Forms.PictureBox picCard1;
+        private System.Windows.Forms.PictureBox picCard2;
+        private System.Windows.Forms.PictureBox picCard3;
+        private System.Windows.Forms.PictureBox picCard4;
+        private System.Windows.Forms.PictureBox picCard5;
+        private System.Windows.Forms.CheckBox chkKeep1;
+        private System.Windows.Forms.CheckBox chkKeep2;
+        private System.Windows.Forms.CheckBox chkKeep3;
+        private System.Windows.Forms.CheckBox chkKeep4;
+        private System.Windows.Forms.CheckBox chkKeep5;
+    }
+}
---
+++ Assignment4.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MainForm.CS
+{
+public partial class Assignment4 : Form
+{
+    private const int HAND_SIZE = 5;      // Number of cards in hand
+        private const int NO_CARD = -1;
+    private Deck deck;             
+    private int[] hand = new int[HAND_SIZE];
+    private PictureBox[] cardBoxes;
+    private CheckBox[] keepBoxes; // CheckBoxes to keep cards
+        public Assignment4()
+{
+InitializeComponent();
+    cardBoxes = new PictureBox[] { picCard1, picCard2, picCard3, picCard4, picCard5 };
+    keepBoxes = new CheckBox[] { chkKeep1, chkKeep2, chkKeep3, chkKeep4, chkKeep5 };
+foreach (var pic in cardBoxes)
+pic.Click += CardPicture_Click;
+btnDeal.Click += BtnDeal_Click;
+btnSave.Click += BtnSave_Click; // Event handler for Save button
+            btnLoad.Click += BtnLoad_Click;
+deck = new Deck(imgCards.Images.Count);
+}
+private void Assignment4_Load(object sender, EventArgs e)
+{
+    DealHand(true); // Initial deal with shuffle
+        }
+private void DealHand(bool newShuffle = false)
+{
+if (newShuffle || !keepBoxes.Any(cb => cb.Checked))
+deck.Shuffle();
+for (int i = 0; i < HAND_SIZE; i++)
+{
+if (!keepBoxes[i].Checked)
+hand[i] = deck.DrawCard();
+}
+UpdateHandDisplay();
+}
+private void UpdateHandDisplay()
+{
+for (int i = 0; i < HAND_SIZE; i++)
+{
+if (hand[i] >= 0 && hand[i] < imgCards.Images.Count)    // Valid card index
+                    cardBoxes[i].Image = imgCards.Images[hand[i]];
+else
+cardBoxes[i].Image = null;
+}
+}
+private void CardPicture_Click(object sender, EventArgs e)
+{
+for (int i = 0; i < HAND_SIZE; i++)
+{
+if (sender == cardBoxes[i])
+keepBoxes[i].Checked = !keepBoxes[i].Checked;
+}
+}
+private void BtnDeal_Click(object sender, EventArgs e)
+{
+DealHand(false);
+}
+private void BtnSave_Click(object sender, EventArgs e)
+{
+using (SaveFileDialog dlg = new SaveFileDialog())
+{
+dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+dlg.DefaultExt = "txt";
+dlg.Filter = "Text files (*.txt)|*.txt"; // Filter for text files
+                if (dlg.ShowDialog() == DialogResult.OK)
+{
+File.WriteAllLines(dlg.FileName, hand.Select(c => c.ToString()));
+MessageBox.Show("Hand saved successfully!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+}
+}
+}
+private void BtnLoad_Click(object sender, EventArgs e)
+{
+using (OpenFileDialog dlg = new OpenFileDialog())
+{
+dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // copilot helped add this line / document add 
+                dlg.DefaultExt = "txt";
+dlg.Filter = "Text files (*.txt)|*.txt";
+if (dlg.ShowDialog() == DialogResult.OK)
+{
+string[] lines = File.ReadAllLines(dlg.FileName);
+for (int i = 0; i < HAND_SIZE; i++) // copilot helped fix loading issue
+                    {
+if (i < lines.Length && int.TryParse(lines[i], out int cardIndex))
+hand[i] = cardIndex;
+else
+hand[i] = NO_CARD;
+keepBoxes[i].Checked = false;
+}
+UpdateHandDisplay();
+MessageBox.Show("Hand loaded successfully!", "Load", MessageBoxButtons.OK, MessageBoxIcon.Information); // Added success message (copilot helped) just extra bonus for when loading
+                }
+}
+}
+}
+public class Deck
+{
+private List<int> cards;
+private Random rng; // Random number generator
+        public Deck(int deckSize)
+{
+rng = new Random();
+cards = Enumerable.Range(0, deckSize).ToList();
+}
+public void Shuffle()
+{
+for (int i = cards.Count - 1; i > 0; i--)
+{
+int j = rng.Next(i + 1);
+int temp = cards[i];
+cards[i] = cards[j];
+cards[j] = temp;
+}
+}
+public int DrawCard()
+{
+            // Return -1 if deck is empty
+            if (cards.Count == 0) return -1;
+    int card = cards[0];
+    cards.RemoveAt(0); // Remove drawn card from deck
+            return card;
+}
+}
+}
